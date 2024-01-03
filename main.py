1788df8d3f03bcbc8fe00c94e79522a01d86ce23
Luc VIGNOLLES <vignolles.luc@gmail.com>
2024-01-03 18:36:47 +0100

    code refactor done

--- main.py
+++ main.py
@@ -14,121 +14,113 @@ IM_RAW_W = 836
 IM_RAW_H = 1200
 
 IM_W = 210
-IM_H = IM_RAW_H*IM_RAW_W*210
+IM_H = (IM_RAW_H/IM_RAW_W)*210
 
 PREFIX = ""
 SUFFIX = ".rawkuma.com.jpg"
 
 BASE_CHAP_URL = "https://rawkuma.com/komi-san-wa-komyushou-desu-chapter-"
+TRAGET_DIR_PATH = r"D:/Books/" + NAME + "/"
 
-def get_dl_ids(base_chap_url, chap_list):
+
+def get_dl_url(driver, chap_url):
     """
-    Visits rawkuma.com using a selenium driver for every chapter number in chap_list. 
-    Then the download button is located and analysed to retrieve the dl url
-    base_chap_url: str
-    chap_list: [str]
+    Visits rawkuma.com using a selenium driver to get the dl url for a specified chapter
+    driver: a selenium webdriver
+    chap_url: str
     """
-    driver = webdriver.Firefox(executable_path = "geckodriverv033/geckodriver.exe")
 
-    dl_urls = []
+    print("Accessing " + chap_url, end = ': ')
 
-    for chap in chap_list:
+    #find element and get url
+    try:
+        driver.get(chap_url)
+        dl_elem = driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/article/div[4]/div[2]/span[3]/span[3]/a")
+        url = dl_elem.get_attribute("href")
+        print("Ok")
+    except Exception as e:
+        url = ""
+        print(e)
 
-        print("Accessing chapter " + chap, end = ': ')
+    return url
 
-        #find element and get url
-        try:
-            driver.get(base_chap_url + chap)
-            dl_elem = driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/article/div[4]/div[2]/span[3]/span[3]/a")
-            dl_urls.append(dl_elem.get_attribute("href"))
-        except Exception as e:
-            print(e)
+def download_from_url(url, chap_number):
 
-        print("dl url obtained")
+    response = requests.get(url)
+    chap_path = TRAGET_DIR_PATH + "downloads/" + NAME + "_chap_" + chap_number + ".zip"
 
-    driver.quit()
+    print("Downloading at " + url, end= ': ')
+    print(response.reason)
 
-    return dl_urls
+    with open(chap_path, 'wb') as file:
+        file.write(response.content)
 
-def download_from_website(url_list, chap_list):
+    response.close()
 
-    paths = []
+    return chap_path
 
-    for url, chap in zip(url_list, chap_list):
-        response = requests.get(url)
-        chap_path = NAME + "_chap_" + chap + ".zip"
+def unzip_to_pdf(chap_zip_path, chap_number):
+    with zipfile.ZipFile(chap_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(chap_zip_path[:-4])
 
-        print("Downloading at " + url, end= ': ')
-        print(response.reason)
-        response.close()
+    os.chdir(chap_zip_path[:-4])
 
-        with open(chap_path, 'wb') as file:
-            file.write(response.content)
-        time.sleep(5)
+    # PDF setup
+    pdf = FPDF('P', 'mm', (IM_W, IM_H))
+    pdf.set_top_margin(0)
+    pdf.set_left_margin(0)
+    pdf.set_auto_page_break(0)
+    pdf.set_display_mode('fullpage', 'single')
 
-        paths.append(chap_path)
+    num = 0
+    image_path = PREFIX + str(num) + SUFFIX 
+    while os.path.exists(image_path):
+        cv_img = cv2.imread(image_path)
 
-    return paths
+        h = len(cv_img)
+        w = len(cv_img[0])
 
-def unzip_to_pdf(paths):
-    for path in paths:
-        with zipfile.ZipFile(path, 'r') as zip_ref:
-            zip_ref.extractall(path[:-4])
-
-        os.chdir(path[:-4])
-
-        # PDF setup
-        pdf = FPDF('P', 'mm', (IM_W, IM_H))
-        pdf.set_top_margin(0)
-        pdf.set_left_margin(0)
-        pdf.set_auto_page_break(0)
-        pdf.set_display_mode('fullpage', 'single')
-
-        num = 0
-        image_path = PREFIX + str(num) + SUFFIX 
-        while os.path.exists(image_path):
-            cv_img = cv2.imread(image_path)
-
-            h = len(cv_img)
-            w = len(cv_img[0])
-
-            if h > w:
-                pdf.add_page('P')
-                if w/h > IM_W/IM_H:
-                    pdf.image(image_path, w = IM_W)
-                else:
-                    pdf.image(image_path, h = IM_H)
-
+        if h > w:
+            pdf.add_page('P')
+            if w/h > IM_W/IM_H:
+                pdf.image(image_path, w = IM_W)
             else:
-                pdf.add_page('L')
-                if w/h > IM_W/IM_H:
-                    pdf.image(image_path, h = IM_H)
-                else:
-                    pdf.image(image_path, w = IM_W)
-            num = num + 1
-            image_path = PREFIX + str(num) + SUFFIX
+                pdf.image(image_path, h = IM_H)
 
-        # Make new directory in books if not created
+        else:
+            pdf.add_page('L')
+            if w/h > IM_W/IM_H:
+                pdf.image(image_path, h = IM_H)
+            else:
+                pdf.image(image_path, w = IM_W)
+        num = num + 1
+        image_path = PREFIX + str(num) + SUFFIX
 
-        print("Generated chapter at " + "D:/Books/" + NAME + "/" + path[:-4] + ".pdf")
-        os.chdir('..')
-        pdf.output(r"D:/Books/" + NAME + "/" + path[:-4] + ".pdf")
+    print("Generated chapter at " + TRAGET_DIR_PATH + NAME + "_chap_" + chap_number + ".pdf")
+    os.chdir("..")
+    pdf.output(TRAGET_DIR_PATH + NAME + "_chap_" + chap_number + ".pdf")
 
 if __name__ == "__main__":
 
-    if not os.path.exists(r"D:/Books/" + NAME):
-            os.mkdir(r"D:/Books/" + NAME)
+    driver = webdriver.Firefox(executable_path = r"C:/Program Files/GeckoDriver/geckodriver.exe")
 
-    chap_list = []
+    if not os.path.exists(TRAGET_DIR_PATH):
+        os.mkdir(TRAGET_DIR_PATH)
 
-    for i in range(160,436):
-        chap_list.append(f"{i:02}")
+    if not os.path.exists(TRAGET_DIR_PATH + "downloads/"):
+        os.mkdir(TRAGET_DIR_PATH + "downloads/")
 
-    urls = get_dl_ids(BASE_CHAP_URL, chap_list)
+    chap_list = [str(i) for i in range(384, 436)]
 
-    os.chdir("./downloads")
-    paths = download_from_website(urls, chap_list)
+    for chap in chap_list:
+        chap_loc_path = download_from_url(get_dl_url(driver, BASE_CHAP_URL + chap), chap)
+        unzip_to_pdf(chap_loc_path, chap)
 
-    unzip_to_pdf(paths)
+    driver.quit()
+
+    # urls = get_dl_ids(BASE_CHAP_URL, chap_list)
+    # paths = download_from_website(urls, chap_list)
+
+    # unzip_to_pdf(paths)
 
 
